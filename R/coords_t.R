29f10c945724b94e972c7b66c87efff0f4929c60
mmyrte <24587121+mmyrte@users.noreply.github.com>
2025-09-09 18:48:50 +0200

    #5 move coords creation to population method

--- R/coords_t.R
+++ R/coords_t.R
@@ -6,8 +6,7 @@
 #'
 #' @name coords_t
 #'
-#' @param config An object of class "evoland_config" containing coordinate
-#'   system specifications in the coords field.
+#' @param evoland_db An [evoland_db] instance
 #'
 #' @return A data.table of class "coords_t" with columns:
 #'   - `id_coord`: Unique ID for each coordinate pair
@@ -15,11 +14,10 @@
 #'   - `lat`: Latitude/y coordinate
 #'   - `elevation`: Elevation (initially NULL)
 #'   - `region`: Region designation (initially NULL)
-#'   - `geom_point`: Geometry point object
 #'   - `geom_polygon`: Geometry polygon object (for grid cells)
 #' @export
-create_coords_t <- function(config) {
-  stopifnot(inherits(config, "evoland_config"))
+populate_coords_t <- function(evoland_db) {
+  config <- evoland_db$config
   coords_spec <- config[["coords"]]
   coord_type <- coords_spec[["type"]]
 
@@ -32,9 +30,81 @@ create_coords_t <- function(config) {
     ))
   )
 
-  validate(out)
+  out <- validate(out)
+
+  evoland_db$commit(out, "coords_t", mode = "overwrite")
 }
 
+#' @export
+validate.coords_t <- function(x, ...) {
+  # Check that it's a data.table
+  if (!inherits(x, "data.table")) {
+    stop("coords_t must inherit from data.table")
+  }
+
+  # Check required columns
+  required_cols <- c("id_coord", "lon", "lat", "elevation", "region", "geom_polygon")
+  missing_cols <- setdiff(required_cols, names(x))
+  if (length(missing_cols) > 0) {
+    stop(glue::glue(
+      "coords_t missing required columns: {paste(missing_cols, collapse = ', ')}"
+    ))
+  }
+
+  # Check column types
+  if (!is.integer(x[["id_coord"]])) {
+    id_ints <- as.integer(x[["id_coord"]])
+    if (anyNA(id_ints)) {
+      stop("id_coord must be int or coercible to it")
+    }
+    data.table::set(x, j = "id_coord", value = id_ints)
+  }
+
+  if (!is.numeric(x[["lon"]]) || !is.numeric(x[["lat"]])) {
+    stop("lon and lat must be numeric")
+  }
+
+  if (!is.factor(x[["region"]])) {
+    data.table::set(
+      x,
+      j = "region",
+      value = as.factor(x[["region"]])
+    )
+    warning("coerced region to factor")
+  }
+
+  # Check for unique id_coord values
+  if (anyDuplicated(x[["id_coord"]])) {
+    stop("id_coord values must be unique")
+  }
+
+  if (!inherits(x[["geom_polygon"]], "sfc")) {
+    warning("`geom_polygon` is not a simple feature collection")
+  }
+  if (all(is.na(x[["elevation"]]))) {
+    # TODO insert name of method for sampling / setting elevation
+    warning("`elevation` is all NA, populate using ")
+  }
+  if (all(is.na(x[["region"]]))) {
+    # TODO insert name of method for sampling / setting region
+    warning("`region` is all NA, populate using ")
+  }
+
+  return(x)
+}
+
+#' @export
+print.coords_t <- function(x, ...) {
+  cat(glue::glue(
+    "Coordinate Table\n",
+    "longitude (x) range: [{range(x[['lon']])}]\n",
+    "latitude  (y) range: [{range(x[['lat']])}]\n\n"
+  ))
+  NextMethod(...)
+  invisible(x)
+}
+
+# Create a new coords_t_square from specs
 create_coords_t_square <- function(coords_spec) {
   stopifnot(
     # Validate required fields
@@ -84,85 +154,11 @@ create_coords_t_square <- function(coords_spec) {
 
   # Add S3 classes
   class(base_grid_dt) <- c(
-    "square_coords_t",
+    "coords_t_square",
     "coords_t",
     "evoland_t",
     class(base_grid_dt)
   )
 
-  attr(base_grid_dt, "epsg") <- coords_spec[["epsg"]]
-  attr(base_grid_dt, "xmin") <- coords_spec[["extent"]][["xmin"]]
-  attr(base_grid_dt, "xmax") <- coords_spec[["extent"]][["xmax"]]
-  attr(base_grid_dt, "ymin") <- coords_spec[["extent"]][["ymin"]]
-  attr(base_grid_dt, "ymax") <- coords_spec[["extent"]][["ymax"]]
-  attr(base_grid_dt, "resolution") <- coords_spec[["resolution"]]
-
   return(base_grid_dt)
-}
-
-#' @export
-validate.coords_t <- function(x, ...) {
-  # Check that it's a data.table
-  if (!inherits(x, "data.table")) {
-    stop("coords_t must inherit from data.table")
-  }
-
-  if (!rlang::is_scalar_integerish(attr(x, "epsg"))) {
-    stop("EPSG attribute must be integerish")
-  }
-
-  # Check required columns
-  required_cols <- c("id_coord", "lon", "lat", "elevation", "region", "geom_polygon")
-  missing_cols <- setdiff(required_cols, names(x))
-  if (length(missing_cols) > 0) {
-    stop(glue::glue(
-      "coords_t missing required columns: {paste(missing_cols, collapse = ', ')}"
-    ))
-  }
-
-  # Check column types
-  if (!is.integer(x[["id_coord"]])) {
-    stop("id_coord must be integer")
-  }
-
-  if (!is.numeric(x[["lon"]]) || !is.numeric(x[["lat"]])) {
-    stop("lon and lat must be numeric")
-  }
-
-  if (!is.factor(x[["region"]])) {
-    stop("region must be factor")
-  }
-
-  # Check for unique id_coord values
-  if (anyDuplicated(x[["id_coord"]])) {
-    stop("id_coord values must be unique")
-  }
-
-  if (!inherits(x[["geom_polygon"]], "sfc")) {
-    warning("`geom_polygon` is not a simple feature collection")
-  }
-  if (all(is.na(x[["elevation"]]))) {
-    # TODO insert name of method for sampling / setting elevation
-    warning("`elevation` is all NA, populate using ")
-  }
-  if (all(is.na(x[["region"]]))) {
-    # TODO insert name of method for sampling / setting region
-    warning("`region` is all NA, populate using ")
-  }
-
-  return(x)
-}
-
-#' @export
-print.square_coords_t <- function(x, ...) {
-  cat(glue::glue(
-    "Square Coordinate Table; CRS: EPSG:{attr(x, 'epsg')}\n",
-    "Dimensions: ",
-    "{attr(x, 'xmax') - attr(x, 'xmin')} by {attr(x, 'ymax') - attr(x, 'ymin')}; ",
-    "Resolution: {attr(x, 'resolution')}\n",
-    "longitude (x) range: [{attr(x, 'xmin')}, {attr(x, 'xmax')}]\n",
-    "latitude  (y) range: [{attr(x, 'ymin')}, {attr(x, 'ymax')}]\n\n"
-  ))
-  NextMethod(...)
-  invisible(x)
 }
--- inst/tinytest/test_coords_t.R
+++ inst/tinytest/test_coords_t.R
@@ -1,15 +1,10 @@
 library(tinytest)
 devtools::load_all()
 
-config_path <- system.file("config.yml", package = "evoland")
-config <- read_evoland_config(config_path)
+config_path <- system.file("config.json", package = "evoland")
+db <- evoland_db$new(":memory:")
+ingest_evoland_config(db, config_path)
 
 # expecting the validator to complain that fields are all-na
-expect_warning(
-  square_coords_t <- create_coords_t(config),
-  "is all NA, populate using"
-)
-
-expect_equal(nrow(square_coords_t), 8280000L)
-
-expect_stdout(print(square_coords_t), "CRS: EPSG:2056")
+expect_warning(populate_coords_t(db), "is all NA, populate using")
+expect_equal(db$row_count("coords_t"), 10000L)
