c0bcc013c327abb345603fc8114950e637a6b9bb
mmyrte <24587121+mmyrte@users.noreply.github.com>
2026-03-03 12:27:56 +0100

    evoland_db_util: add get_read_expr for fallthrough reads

--- R/evoland_db_util.R
+++ R/evoland_db_util.R
@@ -1,9 +1,131 @@
-#' Run parallel tasks on cloned `evoland_db` instances
+#' Utility functions and methods for `evoland_db`
 #'
-#' This wrapper takes an (optional) worker cluster, a worker function, a set of items,
-#' and a parent `evoland_db` instance. If **no** cluster is passed, apply the worker
-#' serially. **If** a cluster is passed, each worker gets its own database instance
-#' derived from the parent database.
+#' Factored out utility functions and methods specifically used in conjunction with
+#' `evoland_db` and its subclasses.
+#'
+#' @name evoland_db_util
+NULL
+
+#' @describeIn evoland_db_util Gets the read expression for a given table, taking into
+#' account the run lineage and distinctness columns. If the table has no `id_run` column
+#' or is the `runs_t` table itself, falls back to the default read expression.
+#' Otherwise, constructs a read expression that filters for the active `id_run` and its lineage,
+#' using the distinctness columns to determine which slices of data to include.
+#' @param self The `evoland_db` instance
+#' @param table_name The name of the table to read from
+#' @return A SQL expression string to read most specific data for slice
+get_evoland_db_read_expr <- function(self, super, table_name) {
+  table_path <- super$get_table_path(table_name)
+  all_cols <- self$get_query(
+    glue::glue("select column_name from (describe '{table_path}')")
+  )[[1]]
+
+  if (
+    !("id_run" %in% all_cols) || # no id_run
+      table_name == "runs_t" # runs_t table itself
+  ) {
+    return(super$get_read_expr(table_name))
+  }
+
+  # Hardcode the set of columns to probe for distinctness; i.e. if any value is present
+  # for a given distinctness tuple, the corresponding slice of data will be used. This
+  # is a tradeoff: we could also choose to fall through on each coordinate point, but
+  # that would blow up the query time.
+  theoretical_distinctness_cols <- c("id_run", "id_period", "id_pred")
+  distinctness_cols <- intersect(all_cols, theoretical_distinctness_cols)
+  inheritance_key_cols <- setdiff(distinctness_cols, "id_run")
+
+  # Single run in lineage, or only id_run present: just filter for active id_run
+  if (length(self$run_lineage) == 1L || identical(distinctness_cols, "id_run")) {
+    stopifnot("id_run needs to be set" = !is.null(self$id_run))
+    # check for nullness - else we'd get back an empty char vector
+    return(glue::glue(
+      "(select * from '{table_path}' where id_run = {self$id_run})"
+    ))
+  }
+
+  # map each id_run in lineage to its distance from the active run; used to
+  # find the minimum distance. e.g. if run_lineage is (3, 2, 0) we get
+  # case id_run when 3 then 1 when 2 then 2 when 0 then 3 else 999999 end
+  run_case <- glue::glue(
+    "case b.id_run ",
+    paste(
+      glue::glue("when {self$run_lineage} then {seq_along(self$run_lineage)}"),
+      collapse = " "
+    ),
+    " else 999999 end"
+  )
+
+  ctes <- list()
+
+  # return one row per tuple of available data within lineage
+  ctes[["data_present"]] <- glue::glue(
+    r"[
+    select distinct
+      {toString(distinctness_cols)}
+    from
+      '{table_path}'
+    where
+      id_run in ({toString(self$run_lineage)})
+    ]"
+  )
+
+  join_conditions <- vapply(
+    inheritance_key_cols,
+    function(col) {
+      if (col == "id_period") {
+        # special case for id_period: self-join data_present to allow for
+        # id_period=0 fallback
+        return(glue::glue("(a.{col} = b.{col} or b.{col} = 0)"))
+      }
+      glue::glue("a.{col} = b.{col}")
+    },
+    character(1)
+  )
+
+  # reduce data_present to one row per most specific id_run
+  ctes[["best_run"]] <- glue::glue(
+    r"[
+    select
+      {paste0("b.", inheritance_key_cols, collapse = ", ")},
+      -- arg_min returns id_run for the single row where run_case is minimal
+      arg_min(b.id_run, {run_case}) as id_run
+    from
+      data_present a,
+      data_present b
+    where
+      {paste(join_conditions, collapse = " and ")}
+    group by
+      {paste0("b.", inheritance_key_cols, collapse = ", ")}
+    ]"
+  )
+
+  # return read expression: use semi join to filter table_path using best_run
+  glue::glue(
+    r"[(
+    with
+      data_present as (
+        {ctes[["data_present"]]}
+      ),
+      best_run as (
+        {ctes[["best_run"]]}
+      )
+    from
+      '{table_path}' c
+    semi join
+      best_run b
+      using ({toString(distinctness_cols)})
+    where
+      c.id_run in ({toString(self$run_lineage)})
+    )]"
+  )
+}
+
+#' @describeIn evoland_db_util Run a worker function in parallel with an `evoland_db`
+#' instance; takes an (optional) worker cluster, a worker function, a set of
+#' items, and a parent `evoland_db` instance. If **no** cluster is passed, apply the
+#' worker serially. **If** a cluster is passed, each worker gets its own database
+#' instance derived from the parent database.
 #'
 #' @param worker_fun A function to apply to each item, with signature `worker_fun(item,
 #' db, ...)`. The `db` argument will be a new `evoland_db` instance for each worker.
